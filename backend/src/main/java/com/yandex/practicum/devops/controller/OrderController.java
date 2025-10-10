502288f1f4094d6e8f341c4f08aa3a5f1659bc26
¨Aquid¨ <aquid89@gmail.com>
2025-10-11 00:39:55 +0500

    fix

--- backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
+++ backend/src/main/java/com/yandex/practicum/devops/controller/OrderController.java
@@ -10,7 +10,7 @@ import com.yandex.practicum.devops.service.OrderService;
 import com.yandex.practicum.devops.service.ProductService; 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity; 
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
